337fdbefbd897f5dcf852f29e9cc29e2c67ac89f
Ethan-23 <emanroberts@gmail.com>
2021-12-10 01:43:25 +0000

    added task 10

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""Task 10"""
 
 
 def poly_derivative(poly):
     """poly derivative"""
     new = []
+    if poly == [0]:
+        return [0]
     for i in range(len(poly)):
         if i != 0:
             if poly[i] == 0:
