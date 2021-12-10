fa1445b1f7873c395c27adb6ead3e0b255378233
Ethan-23 <emanroberts@gmail.com>
2021-12-10 01:12:16 +0000

    added task 9

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Something"""
+
+
+def summation_i_squared(n):
+    """summation_i_squared"""
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i * i
+    return sum
