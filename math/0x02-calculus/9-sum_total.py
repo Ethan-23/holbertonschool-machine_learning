1afc95b4e3f13c3412a8d153e339882f31af144b
Ethan-23 <emanroberts@gmail.com>
2021-12-10 01:29:49 +0000

    test push

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,8 +4,5 @@
 
 def summation_i_squared(n):
     """summation_i_squared"""
-    if type(n) == int:
-        sum = (n * (n+1) * (2*n+1)) / 6
-        return int(sum)
-    else:
-        return None
+    sum = (n * (n+1) * (2*n+1)) / 6
+    return int(sum)
