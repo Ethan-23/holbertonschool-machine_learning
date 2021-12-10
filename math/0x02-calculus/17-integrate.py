926f9843871d6583efae1c818bfa086fc048c063
Ethan-23 <emanroberts@gmail.com>
2021-12-10 02:58:41 +0000

    added task 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""task 17"""
+
+
+def poly_integral(poly, C=0):
+    """Integrate"""
+    if type(poly) != list or len(poly) == 0:
+        return None
+    if len(poly) == 1:
+        return [0]
+    integral = [0]
+    for i in range(len(poly)):
+        if poly[i] == 0:
+            integral.append(poly[i])
+        else:
+            integral.append(poly[i] / (i + 1))
+    return integral
