2d93d169c63cca8bc824befbf64ed7eb99a5c86a
Ethan-23 <emanroberts@gmail.com>
2021-12-10 01:23:38 +0000

    added task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """summation_i_squared"""
-    if type(n) is int:
+    if type(n) == int:
         return (n * (n+1) * (2*n+1)) / 6
     else:
         return None
