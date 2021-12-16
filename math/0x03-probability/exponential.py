45fea4822b344365dd6b96fa108ed75691821679
Ethan-23 <emanroberts@gmail.com>
2021-12-16 02:52:54 +0000

    updated labtha equation

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -18,7 +18,7 @@ class Exponential:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = sum(data) / len(data)
+            self.lambtha = 1 / (sum(data) / len(data))
 
     def pmf(self, k):
         """Exponential PMF"""
