c198f724a683cb68ff7bb828f67b0377271fe8ee
Ethan-23 <emanroberts@gmail.com>
2021-12-27 01:57:20 +0000

    added task 0 and README

---
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class Neuron:
+    """Neuron Class"""
+
+    def __init__(self, nx):
+        """
+        Init for Neuron Class
+        nx: is the number of input features to the neuron
+        """
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.normal(size=(1,nx))
+        self.b = 0
+        self.A = 0
