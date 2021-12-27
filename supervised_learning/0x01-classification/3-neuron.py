1b4ea828e2a0faff50007254f21671faea49691d
Ethan-23 <emanroberts@gmail.com>
2021-12-27 03:36:40 +0000

    added task 3

---
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class Neuron:
+    """Neuron Class"""
+
+    def __init__(self, nx):
+        """
+        Init for Neuron Class
+        nx: is the number of input features to the neuron
+        """
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+    
+    @property
+    def W(self):
+        """Getter for W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for A"""
+        return self.__A
+
+    def sigmoid(self, z):
+        return 1/(1 + np.exp(-z))
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron
+        X is a numpy.ndarray with shape (nx, m) that contains the input data
+        - nx is the number of input features to the neuron
+        - m is the number of examples
+        """
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = self.sigmoid(Z)
+        return self.A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression
+        Y is a numpy.ndarray with shape (1, m) that contains the correct labels for the input data
+        A is a numpy.ndarray with shape (1, m) containing the activated output of the neuron for each example
+        """
+        loss = -(Y*np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = np.sum(1/Y.shape[1] * loss)
+        return cost
