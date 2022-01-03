17a8028fdb98184836141bacbe2f216f050c6628
Ethan-23 <emanroberts@gmail.com>
2022-01-03 03:16:09 +0000

    added task 24

---
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """
+    Y is a numpy.ndarray with shape (m,) containing numeric class labels
+    - m is the number of examples
+    classes is the maximum number of classes found in Y
+    Returns: a one-hot encoding of Y with shape (classes, m), or None
+        on failure
+    """
+    size = (classes, len(Y))
+    one_hot = np.zeros(size)
+    rows = np.arange(classes)
+    one_hot[Y, rows] = 1
+    return one_hot
