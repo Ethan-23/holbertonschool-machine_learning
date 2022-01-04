0f2f9907c7d1c894f3dd3e8b918697eb4a059ae5
Ethan-23 <emanroberts@gmail.com>
2022-01-04 23:38:48 +0000

    added task 28

--- supervised_learning/0x01-classification/28-deep_neural_network.py
+++ supervised_learning/0x01-classification/28-deep_neural_network.py
@@ -85,7 +85,7 @@ class DeepNeuralNetwork:
                 self.cache[a] = (t / np.sum(t, axis=0, keepdims=True))
             else:
                 if self.activation == "sig":
-                    self.cache[a] = (1 / (1 + np.exp(-Z)))
+                    self.cache[a] = sigmoid(Z)
                 else:
                     self.cache[a] = np.tanh(Z)
             prev = self.cache[a]
