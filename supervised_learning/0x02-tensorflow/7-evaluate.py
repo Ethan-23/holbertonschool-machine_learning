a1470bb036f4f72d1b8d1fe0cfb6d8aeeeb8c825
Ethan-23 <emanroberts@gmail.com>
2022-01-07 04:17:46 +0000

    testing if wc is failing task

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -19,16 +19,7 @@ def evaluate(X, Y, save_path):
         y_pred = tf.get_collection('y_pred')[0]
         accuracy = tf.get_collection('accuracy')[0]
         loss = tf.get_collection('loss')[0]
-        prediction = sess.run(
-            y_pred,
-            feed_dict={x: X, y: Y}
-        )
-        accuracy = sess.run(
-            accuracy,
-            feed_dict={x: X, y: Y}
-        )
-        loss = sess.run(
-            loss,
-            feed_dict={x: X, y: Y}
-        )
+        prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss = sess.run(loss, feed_dict={x: X, y: Y})
     return (prediction, accuracy, loss)
