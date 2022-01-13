0e3b23eae8d52af83b4541d3cbc6300e0ab32bca
Ethan-23 <emanroberts@gmail.com>
2022-01-13 19:07:23 +0000

    added task 0

---
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" """
+import numpy as np
+
+
+def normalization_constants(X):
+    """
+    X is the numpy.ndarray of shape (m, nx) to normalize
+    - m is the number of data points
+    - nx is the number of features
+    Returns: the mean and standard deviation of each feature, respectively
+    """
+    sdev = np.std(X, axis=0)
+    mean = np.mean(X, axis=0)
+    return mean, sdev
