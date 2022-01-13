f12e698a1ee8ad1428606c0aec5d01b0ac5f4d08
Ethan-23 <emanroberts@gmail.com>
2022-01-13 22:17:55 +0000

    Forgot to import

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" """
+"""12-learningn_rate_decay"""
+import tensorflow.compat.v1 as tf
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
@@ -13,5 +14,10 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     Returns: the learning rate decay operation
     """
     op = tf.train.inverse_time_decay(
-        alpha, global_step, decay_step, decay_rate, staircase=True)
+        alpha,
+        global_step,
+        decay_step,
+        decay_rate,
+        staircase=True
+    )
     return op
