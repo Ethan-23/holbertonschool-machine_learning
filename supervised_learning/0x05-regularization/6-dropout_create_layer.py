e0144a23f6282902f4ff8ad612dea266bfc0e37c
Ethan-23 <emanroberts@gmail.com>
2022-01-21 03:31:52 +0000

    added task 6

---
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Create a Layer with L2 Regularization"""
+
+import tensorflow.compat.v1 as tf
+
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    """
+    prev is a tensor containing the output of the previous layer
+    n is the number of nodes the new layer should contain
+    activation is the activation function that should be used on the layer
+    keep_prob is the probability that a node will be kept
+    Returns: the output of the new layer
+    """
+    kernel_initializer = tf.keras.initializers.VarianceScaling(mode=("fan_avg")
+                                                               )
+    kernel_regularizer = tf.layers.Dropout(keep_prob)
+    layer = tf.layers.Dense(name='layer', units=n, activation=activation,
+                            kernel_initializer=kernel_initializer,
+                            kernel_regularizer=kernel_regularizer
+                            )
+    return layer(prev)
