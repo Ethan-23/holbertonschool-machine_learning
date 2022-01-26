97cc8f8aca73e346147dd493ecaa0f7f5627bb8d
Ethan-23 <emanroberts@gmail.com>
2022-01-26 03:35:19 +0000

    Added task 13

---
+++ supervised_learning/0x06-keras/13-predict.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Sequential"""
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """
+    network is the network model to make the prediction with
+    data is the input data to make the prediction with
+    verbose is a boolean that determines if output should be
+        printed during the prediction process
+    Returns: the prediction for the data
+    """
+    return network.predict(data, verbose=verbose)
