b2452326db961926376aa6013fc2dbde7de0e37b
Ethan-23 <emanroberts@gmail.com>
2022-01-27 22:19:05 +0000

    added task 5

---
+++ math/0x04-convolutions_and_pooling/5-convolve.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""Same Convolution"""
+
+import numpy as np
+
+
+def convolve(images, kernels, padding='same', stride=(1, 1)):
+    """
+    images is a numpy.ndarray with shape (m, h, w, c)
+        containing multiple images
+    - m is the number of images
+    - h is the height in pixels of the images
+    - w is the width in pixels of the images
+    - c is the number of channels in the image
+    kernel is a numpy.ndarray with shape (kh, kw, c, nc) containing
+        the kernel for the convolution
+    - kh is the height of the kernel
+    - kw is the width of the kernel
+    - nc is the number of kernels
+    padding is either a tuple of (ph, pw), ‘same’, or ‘valid’
+    - ‘same’, performs a same convolution
+    - ‘valid’, performs a valid convolution
+    - if a tuple:
+     - ph is the padding for the height of the image
+     - pw is the padding for the width of the image
+    stride is a tuple of (sh, sw)
+    - sh is the stride for the height of the image
+    - sw is the stride for the width of the image
+    Returns: a numpy.ndarray containing the convolved images
+    """
+    m, h, w, c = images.shape
+    kw, kh, kc, nc = kernels.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = ((((h - 1) * sh) + kh - h) // 2) + 1
+        pw = ((((w - 1) * sw) + kw - w) // 2) + 1
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    custom_h = ((h + (2 * ph) - kh)//sh) + 1
+    custom_w = ((w + (2 * pw) - kw)//sw) + 1
+
+    convolution = np.zeros((m, custom_h, custom_w, nc))
+
+    image_padded = np.pad(images, ((0, 0), (ph, ph),
+                          (pw, pw), (0, 0)),
+                          'constant', constant_values=0)
+    for k_num in range(nc):
+        kernel_index = kernels[:, :, :, k_num]
+        i = 0
+        for height in range(0, (h + (2 * ph) - kh + 1), sh):
+            j = 0
+            for width in range(0, (w + (2 * pw) - kw + 1), sw):
+                output = np.sum(image_padded[:, height:height + kh,
+                                             width: width + kw,
+                                             :] * kernel_index,
+                                axis=1).sum(axis=1).sum(axis=1)
+                convolution[:, i, j, k_num] = output
+                j += 1
+            i += 1
+    return convolution
