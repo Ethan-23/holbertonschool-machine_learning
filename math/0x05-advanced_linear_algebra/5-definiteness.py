e105d3334b73028b5712fb36121c6fe6f8fda89c
Ethan-23 <emanroberts@gmail.com>
2022-03-17 18:23:46 +0000

    added task 5

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Determinant"""
+
+import numpy as np
+
+
+def definiteness(matrix):
+    """definiteness"""
+    if type(matrix) != np.ndarray:
+        raise TypeError("matrix must be a numpy.ndarray")
+    if matrix.ndim != 2:
+        return None
+    for i in matrix:
+        if len(matrix) != len(i):
+            return None
+    if np.all(np.linalg.eigvals(matrix) > 0):
+        return "Positive definite"
+    if np.all(np.linalg.eigvals(matrix) >= 0):
+        return "Positive semi-definite"
+    if np.all(np.linalg.eigvals(matrix) < 0):
+        return "Negative definite"
+    if np.all(np.linalg.eigvals(matrix) <= 0):
+        return "Negative semi-definite"
+    return "Indefinite"
