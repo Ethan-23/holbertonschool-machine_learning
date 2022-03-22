ffd1109cf4198d54f37c54c7f16260b1bb14ca20
Ethan-23 <emanroberts@gmail.com>
2022-03-22 01:52:11 +0000

    added task 2

---
+++ math/0x06-multivariate_prob/multinormal.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""1. Correlation"""
+
+import numpy as np
+
+
+class MultiNormal:
+
+    def __init__(self, data):
+        """
+        data is a numpy.ndarray of shape (d, n) containing the data set:
+        n is the number of data points
+        d is the number of dimensions in each data point
+        """
+        if type(data) is not np.ndarray or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        n = data.shape[1]
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+        mean = np.mean(data, axis=1, keepdims=True)
+        self.mean = mean
+        cov = (1 / (n - 1)) * np.matmul(data - mean, data.T - mean.T)
+        self.cov = cov
