496b78fe5208981463964169061cbd9c06071356
Ethan-23 <emanroberts@gmail.com>
2022-03-25 00:47:27 +0000

    added task 1 with edge cases

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -19,9 +19,15 @@ def intersection(x, n, P, Pr):
         raise ValueError("x cannot be greater than n")
     if type(P) is not np.ndarray or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    for i in P:
-        if i < 0 or i > 1:
+    if type(Pr) is not np.ndarray or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    for i in range(P.shape[0]):
+        if P[i] < 0 or P[i] > 1:
             raise ValueError("All values in P must be in the range [0, 1]")
+        if Pr[i] < 0 or Pr[i] > 1:
+            raise ValueError("All values in Pr must be in the range [0, 1]")
+    if np.isclose([np.sum(Pr)], [1]) == [False]:
+        raise ValueError("Pr must sum to 1")
     fact = np.math.factorial
     fact_co = fact(n) / (fact(n - x) * fact(x))
     likelihood = fact_co * (P ** x) * ((1 - P) ** (n - x))
