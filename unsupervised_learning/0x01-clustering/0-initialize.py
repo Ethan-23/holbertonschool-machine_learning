1dbb6fa235293939c8fd72029af8e5fd31fcd7a3
Ethan-23 <emanroberts@gmail.com>
2022-03-30 15:50:12 +0000

    added edge cases

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -12,6 +12,10 @@ def initialize(X, k):
     - d is the number of dimensions for each data point
     k is a positive integer containing the number of clusters
     """
+    if type(X) is not np.ndarray or len(X.shape) is not 2:
+        return None
+    if type(k) is not int or k <= 0:
+        return None
     n, d = X.shape
     high = np.max(X, axis=0)
     low = np.min(X, axis=0)
