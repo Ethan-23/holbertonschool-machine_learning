bd9388ccc0dc7ea3c68d8368fbebdc3cdd8f9aed
Ethan-23 <emanroberts@gmail.com>
2022-04-08 03:49:12 +0000

    added task 2

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -8,7 +8,15 @@ def absorbing(P):
     """
     markov_chain
     """
-    n, n1 = P.shape
-    if n != n1:
+    if type(P) is not np.ndarray or len(P.shape) != 2:
+        return False
+    if P.shape[0] != P.shape[1]:
+        return False
+    if np.min(P ** 2) < 0 or np.min(P ** 3) < 0:
+        return False
+    ab_state = np.where(np.diag(P) == 1)
+    if len(ab_state[0]) == P.shape[0]:
+        return True
+    if len(ab_state[0]) == 0:
         return False
     return True
