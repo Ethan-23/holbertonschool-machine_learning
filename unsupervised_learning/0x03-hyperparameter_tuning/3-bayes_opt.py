786737087a4eb101315a498cc3756dd4b46bde49
Ethan-23 <emanroberts@gmail.com>
2022-04-15 03:24:30 +0000

    added class docu

--- unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """3. Initialize Bayesian Optimization"""
 
+
 import numpy as np
 GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
+    """class"""
 
     def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
                  sigma_f=1, xsi=0.01, minimize=True):
