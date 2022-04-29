65eaae690f1a7aae7f5a500111093d59a357ee3d
Ethan-23 <emanroberts@gmail.com>
2022-04-29 02:51:30 +0000

    added task 5

---
+++ supervised_learning/0x0D-RNNs/5-bi_forward.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""0x0D. RNNs"""
+
+import numpy as np
+
+
+class BidirectionalCell:
+    """BidirectionalCell Class"""
+    def __init__(self, i, h, o):
+        """init for BidirectionalCell class"""
+        self.Whf = np.random.normal(size=(h+i, h))
+        self.Whb = np.random.normal(size=(h+i, h))
+        self.Wy = np.random.normal(size=(h * 2, o))
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """forward for BidirectionalCell"""
+        concat = np.concatenate((h_prev, x_t), 1)
+        h_next = np.tanh(np.matmul(concat, self.Whf) + self.bhf)
+        return h_next
