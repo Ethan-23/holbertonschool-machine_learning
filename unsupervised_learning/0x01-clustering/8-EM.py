5bdae82980aac5e06653eb49daa0566a42ea0dd6
Ethan-23 <emanroberts@gmail.com>
2022-06-09 20:12:08 +0000

    added files

---
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""0. Initialize K-means"""
+
+import numpy as np
+initialize = __import__('4-initialize').initialize
+expectation = __import__('6-expectation').expectation
+maximization = __import__('7-maximization').maximization
+
+def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
+    """
+    expectation_maximization
+    """
+    return None, None, None, None, None
