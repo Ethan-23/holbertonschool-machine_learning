da7e64fc3636a731254baa76b5a4c167e6730f48
Ethan-23 <emanroberts@gmail.com>
2022-06-09 20:44:23 +0000

    added code for task 9

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -6,7 +6,37 @@ expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """
-    BIC Function
-    """
-    return None, None, None, None
+    """BIC Function"""
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return (None, None, None, None)
+    if type(iterations) is not int or iterations <= 0:
+        return (None, None, None, None)
+    if type(kmin) is not int or type(kmax) is not int:
+        return (None, None, None, None)
+    if kmax <= 0 or kmax >= X.shape[0]:
+        return (None, None, None, None)
+    if kmin <= 0 or kmin >= X.shape[0] or kmin >= kmax:
+        return (None, None, None, None)
+    if type(tol) is not float or tol <= 0:
+        return (None, None, None, None)
+    if type(verbose) is not bool:
+        return (None, None, None, None)
+    n, d = X.shape
+    best_k = []
+    best_result = []
+    l1 = []
+    b = []
+    for i in range(kmin, kmax + 1):
+        pi, m, S, g, lh = expectation_maximization(X, i, iterations,
+                                                   tol, verbose)
+
+        temp = (d * i) + (i * d * (d + 1) / 2) + i - 1
+        l1.append(lh)
+        best_k.append(i)
+        best_result.append((pi, m, S))
+        BIC = temp * np.log(n) - 2 * lh
+        b.append(BIC)
+    l1 = np.array(l1)
+    b = np.array(b)
+    best = np.argmin(b)
+    return (best_k[best], best_result[best], l1, b)
