59ca0cdcfcd442238c134c7daad7594e808ca1cb
Ethan-23 <emanroberts@gmail.com>
2022-06-22 03:25:34 +0000

    added task 4

---
+++ reinforcement_learning/0x00-q_learning/4-play.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""load_env"""
+
+import gym
+import numpy as np
+
+
+def play(env, Q, max_steps=100):
+    """play def"""
+    current_state = env.reset()
+    done = False
+    env.render()
+    for step in range(max_steps):
+        action = np.argmax(Q[current_state, :])
+        next_state, reward, done, _ = env.step(action)
+        env.render()
+        if done:
+            break
+        current_state = next_state
+    return reward
