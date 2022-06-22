d93a2879589c204e227320a4556e6f9b1243ef97
Ethan-23 <emanroberts@gmail.com>
2022-06-22 03:02:53 +0000

    added task 2

---
+++ reinforcement_learning/0x00-q_learning/2-epsilon_greedy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""load_env"""
+
+import gym
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """epsilon_greedy"""
+    p = np.random.uniform(0, 1)
+    if p < epsilon:
+        action = np.random.randint(Q.shape[1])
+    else:
+        action = np.argmax(Q[state, :])
+    return action
