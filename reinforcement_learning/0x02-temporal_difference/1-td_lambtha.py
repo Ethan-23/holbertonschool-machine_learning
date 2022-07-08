80c289f697223fded1e2072b3fec6b4becd56c91
Ethan-23 <emanroberts@gmail.com>
2022-07-08 00:53:15 +0000

    PlaceHolders

---
+++ reinforcement_learning/0x02-temporal_difference/1-td_lambtha.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""td_lambtha"""
+
+
+def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100,
+               alpha=0.1, gamma=0.99):
+    """td_lambtha"""
+    
+    return V
---
+++ reinforcement_learning/0x02-temporal_difference/2-sarsa_lambtha.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""sarsa_lambtha"""
+
+
+def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
+                  gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
+    """sarsa_lambtha"""
+
+    return Q
