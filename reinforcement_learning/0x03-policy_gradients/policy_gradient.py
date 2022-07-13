9e5ee6d275ae96cae22b8bee378e120f1069b41b
Ethan <70955616+Ethan-23@users.noreply.github.com>
2022-07-13 16:32:54 -0400

    Added task 1

--- reinforcement_learning/0x03-policy_gradients/policy_gradient.py
+++ reinforcement_learning/0x03-policy_gradients/policy_gradient.py
@@ -3,9 +3,23 @@
 
 import numpy as np
 
+
 def policy(matrix, weight):
     """computes to policy with a weight of a matrix"""
     dot = matrix.dot(weight)
     exp = np.exp(dot)
     res = exp / np.sum(exp)
     return(res)
+
+
+def policy_gradient(state, weight):
+    """computes the Monte-Carlo policy gradient
+       based on a state and a weight matrix"""
+    p = policy(state, weight)
+    action = np.random.choice(len(p[0]), p=p[0])
+    s = p.reshape(-1, 1)
+    softmax = np.diagflat(s) - np.dot(s, s.T)
+    dsoftmax = softmax[action, :]
+    dlog = dsoftmax / p[0, action]
+    gradient = state.T.dot(dlog[None, :])
+    return action, gradient
