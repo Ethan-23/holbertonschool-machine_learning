864244bf7e0157975dc93fdfb567841821e8c2bf
Ethan-23 <emanroberts@gmail.com>
2022-08-02 03:57:44 +0000

    added task 31

---
+++ pipeline/0x02-databases/31-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Insert a document in Python"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """insert_school"""
+    doc = mongo_collection.insert_one(kwargs)
+    return(document.inserted_id)
