41c36f900bab7ca69fdbf6688fd56411d15cdeda
Ethan-23 <emanroberts@gmail.com>
2022-08-02 02:58:41 +0000

    added task 8

--- pipeline/0x02-databases/8-genre_id_by_show.sql
+++ pipeline/0x02-databases/8-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Thing that I need
-SELECT s.`title`, g.`genre_id` FROM `tv_genre` AS s INNER JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` ORDER BY s.`title`, g.`genre_id` ASC;
+SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s INNER JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` ORDER BY s.`title`, g.`genre_id` ASC;
