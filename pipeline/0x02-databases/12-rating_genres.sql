fd2abf5de7e6ef9db48bea5389d275d6b3c08286
Ethan-23 <emanroberts@gmail.com>
2022-08-02 03:25:39 +0000

    added task 12

---
+++ pipeline/0x02-databases/12-rating_genres.sql
@@ -0,0 +1,5 @@
+-- Thing that I need
+SELECT g.name AS name, SUM(r.rate) AS rating FROM tv_genres AS g
+       LEFT JOIN tv_show_genres AS t ON g.id = t.genre_id
+       LEFT JOIN tv_show_ratings AS r ON t.show_id = r.show_id
+       GROUP BY g.name ORDER BY rating DESC;
