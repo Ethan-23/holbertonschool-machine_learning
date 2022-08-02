c90479d5109b012419d59e1fdd043b217d368cea
Ethan-23 <emanroberts@gmail.com>
2022-08-02 03:44:42 +0000

    added task 17

---
+++ pipeline/0x02-databases/17-store.sql
@@ -0,0 +1,13 @@
+-- Thing that I need
+DROP TRIGGER IF EXISTS decrease_quantity;
+
+DELIMITER $$
+CREATE TRIGGER decrease_quantity
+       AFTER INSERT
+       ON `orders` FOR EACH ROW
+BEGIN
+        UPDATE items SET quantity = quantity - new.number
+        WHERE items.name=new.item_name;
+END $$
+
+DELIMITER ;
