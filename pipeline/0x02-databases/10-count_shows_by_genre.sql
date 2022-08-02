5e44ff0da29f9a653d5e994cdac6daca15d74a2f
Ethan-23 <emanroberts@gmail.com>
2022-08-02 03:14:26 +0000

    changed spacing

--- pipeline/0x02-databases/10-count_shows_by_genre.sql
+++ pipeline/0x02-databases/10-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Thing that I need
 SELECT g.name AS genre, COUNT(t.show_id) AS number_of_shows
-    FROM tv_genres as g
-    JOIN tv_show_genres AS t
-    ON g.id = t.genre_id
-    GROUP BY g.id
-    ORDER BY number_of_shows DESC;
+       FROM tv_genres AS g
+       JOIN tv_show_genres AS t
+       ON g.id = t.genre_id
+       GROUP BY g.id
+       ORDER BY number_of_shows DESC;
