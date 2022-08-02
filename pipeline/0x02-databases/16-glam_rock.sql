3859d624f1ee55e81e2fe25bb402526f3a3173a3
Ethan-23 <emanroberts@gmail.com>
2022-08-02 03:39:06 +0000

    added task 16

---
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -0,0 +1,4 @@
+-- Thing that I need
+SELECT band_name, IF(split IS NULL, (2020 - formed), (split - formed)) AS lifespan
+       FROM metal_bands WHERE `style` LIKE '%Glam rock%'
+       ORDER BY lifespan DESC;
