2aebe62b854ddd4005128ef8a4c45b0f2b8c14d8
Ethan-23 <emanroberts@gmail.com>
2022-08-02 03:35:13 +0000

    added desc

--- pipeline/0x02-databases/15-fans.sql
+++ pipeline/0x02-databases/15-fans.sql
@@ -1,3 +1,3 @@
 -- Thing that I need
-SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
-       GROUP BY origin ORDER BY nb_fans;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands 
+       GROUP BY origin ORDER BY nb_fans DESC;
